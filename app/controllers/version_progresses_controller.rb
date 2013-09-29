53d124ab626b31766633a0795e2536151061c002
Jenny Fong <chingyifong@gmail.com>
2013-09-29 16:29:20 +0100

    merge changes

--- app/controllers/version_progresses_controller.rb
+++ app/controllers/version_progresses_controller.rb
@@ -1,10 +1,17 @@
 class VersionProgressesController < ApplicationController
-  include VersionProgressesHelper
   unloadable
 
+  include VersionProgressesHelper
+
+  before_filter :require_admin
+
   def index
-    @version = Project.first(:order => 'name asc').versions.last
-    redirect_to version_progress_url(@version)
+    if params[:project_id]
+      @version = Project.find(params[:project_id]).versions.last
+      redirect_to version_progress_url(@version)
+    else
+      @projects = Project.visible.all(:order => 'name asc')
+    end
   end
 
 
@@ -23,15 +30,13 @@ class VersionProgressesController < ApplicationController
     to_date = DateTime.now
 
     if params[:period] == 'from_date'
-      flash.now[:warning] = "Please enter a date" if params[:from_date].blank?
+      flash.now[:warning] = "Please enter a date." if params[:from_date].blank?
       from_date = params[:from_date]
     elsif params[:period] == 'between'
-      flash.now[:warning] = "Please enter a date range" if params[:from_date].blank? || params[:to_date].blank?
+      flash.now[:warning] = "Please enter a date range." if params[:from_date].blank? || params[:to_date].blank?
       from_date = params[:from_date]
       to_date = params[:to_date]
     end
-
-    flash.now[:warning] = "Please enter an estimated development days" if params[:estimated_dev_days].blank?
 
     @version_progresses = VersionProgress.all(:conditions => ["version_id = ? and created_at > ? and created_at < ?", @version.id, from_date, to_date], :order => "created_at desc")
 
@@ -39,7 +44,7 @@ class VersionProgressesController < ApplicationController
 
     if params[:commit] == "Export to CSV"
 
-      send_data(version_progresses_to_csv(@version_progresses, params[:estimated_dev_days]), :type => 'text/csv; header=present', :filename => 'export.csv')
+      send_data(version_progresses_to_csv(@version_progresses), :type => 'text/csv; header=present', :filename => "export_#{DateTime.now.strftime('%Y-%m-%d_%H-%M')}.csv")
     end
 
 
--- app/helpers/version_progresses_helper.rb
+++ app/helpers/version_progresses_helper.rb
@@ -1,13 +1,5 @@
 module VersionProgressesHelper
 
-  def percentage_dev_days(spent_hour, estimated_dev_days)
-    unless estimated_dev_days.to_f == 0.0
-      ((spent_hour/8)/estimated_dev_days.to_f)*100
-    else
-      0
-    end
-
-  end
 
   def display_date(date)
     date.strftime('%Y-%m-%d')
@@ -22,23 +14,22 @@ module VersionProgressesHelper
   end
 
 
-  def version_progresses_to_csv(progresses, estimate_dev_days, options={})
+  def version_progresses_to_csv(progresses, options={})
 
     decimal_separator = l(:general_csv_decimal_separator)
     encoding = l(:general_csv_encoding)
 
 
-    export = FCSV.generate(:col_sep => l(:general_csv_separator)) do |csv|
+    export = FCSV.generate(:col_sep => l(:general_csv_separator), :row_sep => "\r\n") do |csv|
       # csv header fields
 
-      csv << ['Date', '% Done', "% Dev Days", 'Dev Days']
+      csv << ['Date', '% Done', 'Dev Days']
 
       #csv lines
 
       progresses.each do |progress|
-        csv << [display_date(progress.created_at), display_percentage_done(progress.percentage_done), display_percentage_done(percentage_dev_days(progress.spent_hours, estimate_dev_days)), display_spent_days(progress.spent_hours)]
+        csv << [display_date(progress.created_at), display_percentage_done(progress.percentage_done), display_spent_days(progress.spent_hours)]
       end
-
     end
     export
   end
